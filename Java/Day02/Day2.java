81f7c655505f1351e97c933566063ce0a1d04d59
absarrahman <raman040635@gmail.com>
2020-01-03 00:11:17 +0600

    day-2:New task
    Signed-off-by: absarrahman <raman040635@gmail.com>

---
+++ Java/Day02/Day2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if(x==011){
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+    }
+}
